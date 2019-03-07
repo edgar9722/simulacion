f815082c3b27afce6adb43b5642f6fd0f2ff746d
José Darío Olivares Flores <fcdarios>
2019-03-06 19:34:29 -0600

    Aditivo, pide los datos y ya genera los numeros

--- src/Aditivo.java
+++ src/Aditivo.java
@@ -1,53 +1,69 @@
-public class Aditivo {
-    int[] semilla;
-    int modulo;
-    int[] x;
-    int[] numeros;
+import java.util.Scanner;
 
-    public Aditivo(int[] semilla, int modulo, int[] x, int[] numeros) {
-        this.semilla = semilla;
-        this.modulo = modulo;
-        this.x = x;
-        this.numeros = numeros;
+public class Aditivo {
+    int[] semillas;
+    int modulo;
+    int[] numeros;
+    private int cantidadNumeros;
+    private int aux;
+    private int contador = 1;
+    Scanner t = new Scanner (System.in);
+
+    public Aditivo(int cantidadNumeros) {
+        this.cantidadNumeros = cantidadNumeros;
+    }
+
+    // Metodo para pedir los datos necesarios para generar los numeros
+    public void pedirDatos() {
+        boolean terminar = false;
+        int i = 1;
+        int semilla;
+        semillas = new int[cantidadNumeros];
+
+        System.out.println("Ingresa las semillas: ");
+        System.out.println("Semilla "+i+": ");
+        semillas[i-1] = t.nextInt();
+        do {
+            i++;
+            System.out.println("Para terminar escriba 0");
+            System.out.println("Semilla "+i+": ");
+            semilla = t.nextInt();
+            if(semilla==0)
+                terminar= true;
+            else {
+                semillas[i - 1] = semilla;
+                contador++;
+            }
+        }while (!terminar);
+        System.out.println("Ingresa el modulo: ");
+        modulo = t.nextInt();
+    }
+    // Metodo que genera los numeros aletorios
+    public void generarNumeros(){
+        int contadorIzq = 0;
+        int contadorDer = contador-1;
+        numeros = new int[cantidadNumeros];
+        for(int i = 0; i < semillas.length ; i++)
+            numeros[i] = semillas[i];
+
+        for (int i = contador; i < cantidadNumeros; i++){
+            aux = (numeros[contadorIzq]+numeros[contadorDer])%modulo;
+            System.out.println(aux);
+            numeros[i] = aux;
+            contadorIzq++;
+            contadorDer++;
+        }
     }
     // gets and sets
-
-    public int[] getSemilla() {
-        return semilla;
-    }
-
-    public void setSemilla(int[] semilla) {
-        this.semilla = semilla;
+    public int[] getSemillas() {
+        return semillas;
     }
 
     public int getModulo() {
         return modulo;
     }
 
-    public void setModulo(int modulo) {
-        this.modulo = modulo;
-    }
-
-    public int[] getX() {
-        return x;
-    }
-
-    public void setX(int[] x) {
-        this.x = x;
-    }
-
     public int[] getNumeros() {
         return numeros;
     }
-
-    public void setNumeros(int[] numeros) {
-        this.numeros = numeros;
-    }
-    public void ingresarDatos(){
-
-    }
-    public void calcular(){
-
-    }
-    
 }
