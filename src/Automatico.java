ebafd69cbb52a4677230b04f0f041c3395f0b03a
LizMaya <15030502@itcelaya.edu.mx>
2019-03-07 08:19:40 -0600

    Automaticos

---
+++ src/Automatico.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Automatico {
+    int semilla= 6493;
+    int k= 2467;
+    private int cantidadNumeros;
+    private int x;
+    private String aux;
+    private int [] numeros;
+
+    public Automatico(int cantidadNumeros) {
+        this.cantidadNumeros = cantidadNumeros;
+    }
+
+    public void generarNumeros(){
+        x = semilla;
+        numeros = new int[cantidadNumeros];
+        numeros[0] = x;
+        for (int i = 1; i<cantidadNumeros; i++){
+            aux = ""+k*x;
+            while (aux.length() != 8){
+                aux = 0+aux;
+            }
+            x = Integer.parseInt(aux.substring(2,6));
+            numeros[i] = x;
+        }
+    }
+
+    public int getSemilla() {
+        return semilla;
+    }
+    public int getK() {
+        return k;
+    }
+    public int[] getNumeros() {
+        return numeros;
+    }
+}
