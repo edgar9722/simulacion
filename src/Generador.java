761c23729a4d9c90378e1fbdde41ba57b1cd2576
José Darío Olivares Flores <fcdarios>
2019-03-08 16:53:54 -0600

    Se agrego la validacion del periodo de repeticion

--- src/Generador.java
+++ src/Generador.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Generador {
+    Scanner t = new Scanner (System.in);
     int opcion;
     int cant_numeros;
     int[] numeros;
@@ -11,25 +12,27 @@ public class Generador {
     }
 
     public void menu(){
-        Scanner t = new Scanner (System.in);
+
         System.out.println("Elija una opcion para generar los numeros aleatorios:");
         System.out.println("0 Automaticamente: \n" +
-                           "1 Cuadrados centrales: \n" +
-                           "2 Factor constante: \n" +
-                           "3 Producto central: \n" +
-                           "4 Aditivo: \n" +
-                           "5 Mixto: \n" +
-                           "6 Multiplicativo: \n" +
-                           "-> ");
+                "1 Cuadrados centrales: \n" +
+                "2 Factor constante: \n" +
+                "3 Producto central: \n" +
+                "4 Aditivo: \n" +
+                "5 Mixto: \n" +
+                "6 Multiplicativo: \n" +
+                "-> ");
         opcion = t.nextInt();
         System.out.println("¿Cuantos numeros pseudoaleatorios quiere generar? ");
         cant_numeros = t.nextInt();
         llamarFuncion();
     }
     private void llamarFuncion(){
-        Scanner t = new Scanner (System.in);
+
         boolean bandera1 = false;
         int opc;
+
+        // llama la funcion dependiendo la opcion elegida
         switch (opcion){
             case 0: {
                 Automatico automatico = new Automatico(cant_numeros);
@@ -41,8 +44,6 @@ public class Generador {
                 cuadrados_centrales.pedirDatos();
                 cuadrados_centrales.calcular();
                 numeros = cuadrados_centrales.getNumeros();
-
-
             }break;
             case 2: {
                 Factores_Constantes factores_constantes = new Factores_Constantes(cant_numeros);
@@ -62,12 +63,10 @@ public class Generador {
                 numeros = aditivo.getNumeros();
             }break;
             case 5: {
-                 Mixto mixto = new Mixto(cant_numeros);
-                 mixto.pedirDatos();
-                 mixto.generarNumeros();
-                 numeros=mixto.getNumeros();
-
-
+                Mixto mixto = new Mixto(cant_numeros);
+                mixto.pedirDatos();
+                mixto.generarNumeros();
+                numeros=mixto.getNumeros();
             }break;
             case 6: {
 
@@ -76,17 +75,27 @@ public class Generador {
                 System.out.println("Opcion no valida");
             }
         }
-        mostrarNumeros();
-        guardarNumeros();
+        // ---------------------------------------------------
+        // Valida los numeros y si cumple con el periodo de repeticion los imprime y guarda
+        if (validarNumeros()) System.out.println("No cumple con el periodo de repeticion");
+        else {
+            System.out.println("------------------------------------");
+            System.out.println("Cumple con el periodo de repetición");
+            mostrarNumeros();
+            guardarNumeros();
+            System.out.println("Numeros guardados...");
+        }
         System.out.println("------------------------------------\n" +
-                    "1 Regresar al menu\n" +
-                    "Otro Salir" +
-                    "-> ");
+                "1 Regresar al menu\n" +
+                "Otro Salir" +
+                "-> ");
         opc = t.nextInt();
         if(opc == 1)
             menu();
         else System.exit(0);
     }
+
+    // Guardar los numeros y le asigna el nombre dependiendo del metodo
     private void guardarNumeros(){
         Archivos archivos = new Archivos();
         archivos.setNumeros(numeros);
@@ -118,9 +127,26 @@ public class Generador {
         }catch (IOException e){
             System.out.println(e);
         }
-
     }
-
+    private boolean validarNumeros() {
+        System.out.println("----------------------------------------");
+        System.out.println("Periodo de repiticion minimo: ");
+        int periodo = t.nextInt();
+        boolean r = false;
+        int p;
+        int j;
+        for(int i = 0; i < numeros.length; i++){
+            j = i+1;
+            while (j < numeros.length){
+                if(i != j && numeros[i] == numeros[j]){
+                    p = j - i;
+                    if(p < periodo) r = true;
+                }
+                j++;
+            }
+        }
+        return r;
+    }
     private void mostrarNumeros(){
         for(int i = 0; i < numeros.length; i++){
             System.out.println(numeros[i]);
