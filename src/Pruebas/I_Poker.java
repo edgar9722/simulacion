9fc9e288236438685eaff1e10325aedce4a894ad
José Darío Olivares Flores <fcdarios>
2019-03-19 11:46:31 -0600

    Automatico de Factor constante y aditivo

---
+++ src/Pruebas/I_Poker.java
@@ -0,0 +1,16 @@
+package Pruebas;
+
+public class I_Poker {
+    float[] numeros;
+    float sumatoria;
+    float media;
+    float Z;
+    float Za;
+
+    public I_Poker(float[] numeros) {
+        this.numeros = numeros;
+    }
+    boolean calcular(){
+        return false;
+    }
+}
