e020f5264739aa4aea5348ab78584f6ae9321af4
Fer9701 <16030582@itcelaya.edu.mx>
2019-03-21 00:33:35 -0600

    U_Corridas

--- src/Pruebas/I_CorridasAAMedia.java
+++ src/Pruebas/I_CorridasAAMedia.java
@@ -1,4 +1,38 @@
 package Pruebas;
 
 public class I_CorridasAAMedia {
+    int[] numeros;
+    int suma;
+    double media;
+    int corridas_Am=0; // corridas arriba de la media
+    int corridas_Bm=0; // corridas abajo de la media
+
+    public I_CorridasAAMedia(int[] numeros, int suma, double media, int corridas_Am, int corridas_Bm) {
+        this.numeros = numeros;
+        this.suma = suma;
+        this.media = media;
+        this.corridas_Am = corridas_Am;
+        this.corridas_Bm = corridas_Bm;
+    }
+
+    private void Calculos() {
+
+        // obtener el promedio
+        for (int i = 0; i < numeros.length; i++) {
+            suma = suma + numeros[i];
+        }
+        media = suma / numeros.length;
+
+
+        //corridas
+        for (int i = 0; i <numeros.length ;i++ ) {
+            if (numeros[i]>media){
+                corridas_Am=corridas_Am++;
+            }
+            else{
+                corridas_Bm=corridas_Bm++;
+            }
+        }
+
+    }
 }
