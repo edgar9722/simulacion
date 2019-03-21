e8df10eff98c463efa82aebd0962c01b5490f9f8
José Darío Olivares Flores <fcdarios>
2019-03-21 09:55:24 -0600

    Correccion promedios

--- src/Pruebas/I_Huecos.java
+++ src/Pruebas/I_Huecos.java
@@ -1,4 +1,23 @@
 package Pruebas;
 
 public class I_Huecos {
+    int[] numeros;
+    String listDigitos;
+    int[][][] hLongitud;
+    int[] numHuecos;
+
+    public I_Huecos(int[] numeros) {
+        this.numeros = numeros;
+        listDigitos = listarDigitos();
+    }
+
+    String listarDigitos(){
+        String list = "";
+        for (int i = 0; i < numeros.length;i++)
+            list+=""+numeros[i];
+        return list;
+    }
+    void declarar(){
+
+    }
 }
--- src/Pruebas/U_Promedios.java
+++ src/Pruebas/U_Promedios.java
@@ -1,42 +1,36 @@
 package Pruebas;
 
 public class U_Promedios {
-    int[] numeros;
-    int suma;
+    double[] numeros;
+    double suma;
     double media;
     double z;
     boolean validacion = false;
 
-    public U_Promedios(int[] numeros, double media, double z) {
-        this.numeros = numeros;
-        this.media = media;
-        this.z = z;
+    public U_Promedios(int[] numeros) {
+        this.numeros = new double[numeros.length];
+        for (int i = 0; i < numeros.length; i++){
+            this.numeros[i] = numeros[i]/10000;
+        }
         Calculos();
         imprimir();
     }
     private void Calculos(){
-
         // obtener el promedio
         for(int i=0; i< numeros.length; i++)
-        {
           suma=suma+numeros[i];
-        }
         media=suma/numeros.length;
-
        //calcular z
-
+        z = ((media - 0.5)*Math.pow(numeros.length,0.5))/Math.pow(1/12,1/2);
         if(z < 1.96){
-            validacion=true;
+            validacion = true;
         }
     }
     private void imprimir (){
-
         if (validacion )
             System.out.println("Z "+z+" es menor a 1.96 , se acepta la hiposis de que la muestra viene de una distribucion uniforme");
         else
             System.out.println("Z "+z+" es mayor a 1.96 , no se acepta la hipotesis");
-
-
     }
 
 }
