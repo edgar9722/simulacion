18766b80df4300d27794507880b49160ce13d27e
José Darío Olivares Flores <fcdarios>
2019-03-28 10:52:15 -0600

    Huecos

--- src/Pruebas/I_Huecos.java
+++ src/Pruebas/I_Huecos.java
@@ -1,66 +1,159 @@
 package Pruebas;
+import java.util.ArrayList;
 
 public class I_Huecos {
-    int[] numeros;
-    String listDigitos;
-    int[][][] hLongitud;
-    int[] numHuecos;
+    int[] numeros; //Lista de numeros
+    String listDigitos; // Numeros ordenados en fila
+    Arreglo[] longHuecos;
+    Double[] F;
+    Double[] FR;
+    Double[] Sx;
+    Double[] x1;
+    Double[] aFx;
+    Double[] Fx;
+    Double[] FxSx;
+    Double DCritico = 0.136;
+    Double DMax = 0.0;
+    int longMax;
 
     public I_Huecos(int[] numeros) {
         this.numeros = numeros;
+        longHuecos = new Arreglo[10];
         listDigitos = listarDigitos();
+        calcularLongitud();
+        calculos();
+        comprobacion();
+        //mostrar();
     }
 
+    void comprobacion(){
+        System.out.println("D critico: "+DCritico);
+        System.out.println("D MAX |F(x) - S(x)|: "+DMax);
+        if (DMax <= DCritico)
+            System.out.println("Se aprueba la hipotesis nula");
+        else System.out.println("No se aprueba la hipotesis nula");
+    }
+    void calculos (){
+        int inf, sup, saltos, x, inte;
+        boolean band = true;
+        inf = x = 0;
+        inte = 1;
+        saltos = sup = 3;
+        while(sup <= longMax && band){
+            inte ++;
+            inf = sup+1;
+            if (inf+saltos > longMax){
+                sup = longMax;
+                band = false;
+            }
+            else
+                sup = inf + saltos;
+        }
+        F = new Double[inte];
+        FR = new Double[inte];
+        Sx = new Double[inte];
+        x1 = new Double[inte];
+        aFx = new Double[inte];
+        Fx = new Double[inte];
+        FxSx = new Double[inte];
+        inf = x = 0;
+        saltos = sup = 3;
+        for (int k = 0; k<inte; k++){
+            F[k] = 0.0;
+            FR[k] = 0.0;
+            Sx[k] = 0.0;
+            x1[k] = 0.0;
+            aFx[k] = 0.0;
+            Fx[k] = 0.0;
+            FxSx[k] = 0.0;
+            for (int i = 0; i<longHuecos.length; i++){
+                for (int j = 0; j< longHuecos[i].getHuecos().size(); j++ )
+                    x = longHuecos[i].getNumber(j);
+                if (x > inf && x < sup){
+                    F[k]++;
+                    FR[k]+=1/listDigitos.length();
+                    if (k>0) Sx[k] = Sx[k-1]+ FR[k];
+                    else Sx[k] = FR[k];
+                    x1[k]=sup+1.0;
+                    aFx[k] = Math.pow(0.9, x1[k]);
+                    Fx[k] = 1 - aFx[k];
+                    FxSx[k] = Fx[k] - Sx[k];
+                }
+            }
+            inf = sup+1;
+            if (inf+saltos > longMax){
+                sup = longMax;
+            }
+            else
+                sup = inf + saltos;
+        }
+        for (int i = 0; i < inte; i++){
+            if (DMax < FxSx[i])
+                DMax = FxSx[i];
+        }
+    }
+    void calcularLongitud(){
+        int a, b, r, j, i, k;
+        j = k = r = 0;
+        boolean b1 = false;
+        for (i = 0; i < longHuecos.length; i++) {
+            longHuecos[i] = new Arreglo();
+            j = k = 0;
+            while (j < listDigitos.length()){
+                if (i == Integer.parseInt(""+listDigitos.charAt(j))){
+                    a = j;
+                    k = j+1;
+                    b1 = false;
+                    while (k < listDigitos.length() && !b1){
+                        if (i == Integer.parseInt(""+listDigitos.charAt(k))){
+                            b = k;
+                            if(r < b-a-1) r =b-a-1;
+                            longHuecos[i].setNumero(b-a-1);
+                            b1 = true;
+                            j = k;
+                        }
+                        k++;
+                    }
+                }
+                j++;
+            }
+        }
+        longMax = r;
+    }
+    void mostrar(){
+        for (int i = 0; i<longHuecos.length; i++){
+            System.out.println("--- "+i+" ---");
+            for (int j = 0; j< longHuecos[i].getHuecos().size(); j++ )
+                System.out.println((j+1)+" "+longHuecos[i].getHuecos().get(j));
+        }
+    }
     String listarDigitos(){
         String list = "";
         for (int i = 0; i < numeros.length;i++)
             list+=""+numeros[i];
         return list;
     }
-    void declarar(){
-        int[] cantidad = new int[10];
-        int numero = 0;
-        int i;
-        for (i = 0;i< cantidad.length; i++)
-            cantidad[i] = 0;
-        for(i = 0; i<listDigitos.length(); i++){
-            numero = Integer.parseInt(""+listDigitos.charAt(i));
-            switch (numero){
-                case 0:{
-                    cantidad[0]++;
-                }break;
-                case 1:{
-                    cantidad[1]++;
-                }break;
-                case 2:{
-                    cantidad[2]++;
-                }break;
-                case 3:{
-                    cantidad[3]++;
-                }break;
-                case 4:{
-                    cantidad[4]++;
-                }break;
-                case 5:{
-                    cantidad[5]++;
-                }break;
-                case 6:{
-                    cantidad[6]++;
-                }break;
-                case 7:{
-                    cantidad[7]++;
-                }break;
-                case 8:{
-                    cantidad[8]++;
-                }break;
-                default:
-                    cantidad[9]++;
-            }
+    //
+    class Arreglo {
+        ArrayList<Integer> huecos = new ArrayList<Integer>();
+        int num;
+        void setNumero(int n){
+            huecos.add(n);
         }
-        for (i = 0; i<cantidad.length;i++)
-                if (numero < cantidad[i])
-                    numero = cantidad[i];
+        public ArrayList<Integer> getHuecos() {
+            return huecos;
+        }
+        int getNumber(int i){
+            return huecos.get(i);
+        }
 
+        public int getNum() {
+            return num;
+        }
 
+        public void setNum(int num) {
+            this.num = num;
+        }
     }
+    //
 }
